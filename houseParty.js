01e38d5117add888f48619883f42f514f7c852ec
Stoyan Zhelyazkov <tenkisha@gmail.com>
2022-04-04 21:46:54 +0300

    test

---
+++ houseParty.js
@@ -0,0 +1,20 @@
+function houseParty(arrays) {
+
+    let arr = []
+    
+    for(let i = 0;i < arrays.length;i++){
+        arr = arrays[i].split(' ')
+
+        console.log(arr)
+    }
+
+    
+
+
+}
+
+houseParty(['Allie is going!',
+'George is going!',
+'John is not going!',
+'George is not going!']
+);
