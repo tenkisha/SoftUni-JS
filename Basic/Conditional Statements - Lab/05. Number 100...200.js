57e78942a1aea929165d0c940278d8751c903d66
Stoyan Zhelyazkov <tenkisha@gmail.com>
2022-04-11 21:27:37 +0300

    test

---
+++ Basic/Conditional Statements - Lab/05. Number 100...200.js
@@ -0,0 +1,11 @@
+function from100to200(input) {
+    let numm = Number(input[0]);
+    if (numm < 100) {
+        console.log("Less than 100")
+    } else if (numm <= 200) {
+        console.log("Between 100 and 200")
+    } else 
+    console.log("Greater than 200")
+}
+
+from100to200(['201'])
