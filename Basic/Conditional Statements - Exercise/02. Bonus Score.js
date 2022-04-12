ba7b88dbb079f69f0a0e3f198033e23c3764d140
Stoyan Zhelyazkov <tenkisha@gmail.com>
2022-04-12 20:40:08 +0300

    test

---
+++ Basic/Conditional Statements - Exercise/02. Bonus Score.js
@@ -0,0 +1,23 @@
+function bonusScore(input) {
+    let bonus = 0.0;
+    let num = Number(input[0]);
+
+    if (num <= 100) {
+        bonus = bonus + 5;
+    } else if (num > 1000) {
+        bonus = num * 0.10
+    } else {
+        bonus = num * 0.20
+    } 
+    
+    if(num % 2 === 0){
+        bonus = bonus + 1;
+    } else if(num % 10 === 5){
+        bonus = bonus + 2;
+    }    
+        console.log(bonus)
+        console.log(num + bonus)
+
+}
+
+bonusScore(['2703'])
