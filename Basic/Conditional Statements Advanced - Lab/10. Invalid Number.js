c784d801b922d010e981212448f563d2f9a01b92
Stoyan Zhelyazkov <tenkisha@gmail.com>
2022-04-16 10:14:37 +0300

    test

---
+++ Basic/Conditional Statements Advanced - Lab/10. Invalid Number.js
@@ -0,0 +1,14 @@
+function invalidNumber(input) {
+    let number = Number(input[0]);
+
+    if (number >= 100 && number <= 200){
+        
+    } 
+    else if(number == 0){
+    
+    }else {
+        console.log('invalid')
+    }
+}
+
+invalidNumber(['99'])
