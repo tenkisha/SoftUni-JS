77ae6b5ae781dee6c4b57e250f11ad899d0384ec
tenkisha <tenkisha@gmail.com>
2022-04-23 22:05:53 +0300

    exercise

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Lab/06. Theatre Promotions.js
@@ -0,0 +1,37 @@
+function theatrePromotion(day, age) {
+    let price = 0;
+  
+    if (age >= 0 && age <= 122) {
+      switch (day) {
+        case "Weekday":
+          if (age > 18 && age <= 64) {
+            price = 18;
+          } else {
+            price = 12;
+          }
+  
+          break;
+        case "Weekend":
+          if (age > 18 && age <= 64) {
+            price = 20;
+          } else {
+            price = 15;
+          }
+          break;
+        case "Holiday":
+          if (age <= 18) {
+            price = 5;
+          } else if (age <= 64) {
+            price = 12;
+          } else {
+            price = 10;
+          }
+          break;
+      }
+      console.log(price + '$')
+    } else {
+      console.log("Error!");
+    }
+  }
+
+  theatrePromotion('Holiday',28)
