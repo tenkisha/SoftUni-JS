15c35e130afc44186816ab3c731623befff45d63
tenkisha <tenkisha@gmail.com>
2022-04-25 21:05:45 +0300

    exercise

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/06. Print And Sum.js
@@ -0,0 +1,14 @@
+function printAndSum(n1, n2) {
+    let counter = 0;
+    let sum = 0;
+    arr = [];
+    for (let i = n1; i <= n2; i++) {
+      sum += i;
+      arr.push(i);
+      counter++;
+    }
+    console.log(arr.join(" "));
+    console.log(`Sum: ${sum}`);
+  }
+
+  printAndSum(5, 10)
