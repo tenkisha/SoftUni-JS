57ae5dad9167c41418323dedd22b2214c298331d
tenkisha <tenkisha@gmail.com>
2022-04-26 18:25:44 +0300

    exercise

---
+++ Fundamentals/Basic Syntax, Conditional Statements and Loops - Exercise/09.Login.js
@@ -0,0 +1,21 @@
+function login(input) {
+  let userName = input.shift();
+  let counter = 0;
+  for (let el of input) {
+    el = el.split("").reverse().join("");
+
+    counter++;
+    if (counter == 4) {
+        console.log(`User ${userName} blocked!`);
+        break;
+      }
+    if (el == userName) {
+      console.log(`User ${userName} logged in.`);
+    } else {
+      console.log(`Incorrect password. Try again.`);
+      
+    }
+  }
+}
+
+login(["sunny", "rainy", "cloudy", "sunny", "not sunny"]);
