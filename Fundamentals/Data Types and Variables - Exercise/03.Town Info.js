d02e864c28926a0c6cf102718aa1b498bc99f947
tenkisha <tenkisha@gmail.com>
2022-04-28 10:00:21 +0300

    exercise

---
+++ Fundamentals/Data Types and Variables - Exercise/03.Town Info.js
@@ -0,0 +1,5 @@
+function townInfo(townName,population,area){
+    console.log(`Town ${townName} has population of ${population} and area ${area} square km.`)
+}
+
+townInfo('Burgas','200 000','15 000')
