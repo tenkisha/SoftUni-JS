80b685f0fe7a084484b3f3c2596a9c95a060f7ad
tenkisha <tenkisha@gmail.com>
2022-04-28 10:04:00 +0300

    exercise

---
+++ Fundamentals/Data Types and Variables - Exercise/07. Lower or Upper.js
@@ -0,0 +1,10 @@
+function lowOrUp(char){
+
+    if(char === char.toUpperCase()){
+        console.log('upper-case')
+    }else if(char === char.toLowerCase()){
+        console.log('lower-case')
+    }
+}
+
+lowOrUp('L')
