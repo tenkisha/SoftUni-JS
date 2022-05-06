2c0cf22c30d67d61346bb2d71e1e76f79748889f
tenkisha <tenkisha@gmail.com>
2022-05-06 18:29:47 +0300

    exercise

---
+++ Fundamentals/Functions - Exercise/01. Smallest of Three Numbers.js
@@ -0,0 +1,11 @@
+function smallestNumber(num1, num2, num3) {
+  if (num1 <= num3 && num1 <= num2) {
+    console.log(num1);
+  } else if (num2 <= num1 && num2 <= num3) {
+    console.log(num2);
+  } else if (num3 <= num1 && num3 <= num2) {
+    console.log(num3);
+  }
+}
+
+smallestNumber(2, 5, 3);
