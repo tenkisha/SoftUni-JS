525ef8d31392bd1b3d3eb50fda8910b7ebab52cd
tenkisha <tenkisha@gmail.com>
2022-05-09 20:29:21 +0300

    exercise

--- Fundamentals/Functions - Exercise/09. Loading Bar.js
+++ Fundamentals/Functions - Exercise/09. Loading Bar.js
@@ -1,38 +1,38 @@
 function loadingBar(number) {
-    if (number == 0) {
-      console.log(`${number}% [..........] 
+  if (number == 0) {
+    console.log(`${number}% [..........] 
   Still loading...`);
-    } else if (number == 10) {
-      console.log(`${number}% [%.........] 
+  } else if (number == 10) {
+    console.log(`${number}% [%.........] 
   Still loading...`);
-    } else if (number == 20) {
-      console.log(`${number}% [%%........] 
+  } else if (number == 20) {
+    console.log(`${number}% [%%........] 
   Still loading...`);
-    } else if (number == 30) {
-      console.log(`${number}% [%%%.......] 
+  } else if (number == 30) {
+    console.log(`${number}% [%%%.......] 
   Still loading...`);
-    } else if (number == 40) {
-      console.log(`${number}% [%%%%......] 
+  } else if (number == 40) {
+    console.log(`${number}% [%%%%......] 
   Still loading...`);
-    } else if (number == 50) {
-      console.log(`${number}% [%%%%%.....] 
+  } else if (number == 50) {
+    console.log(`${number}% [%%%%%.....] 
   Still loading...`);
-    } else if (number == 60) {
-      console.log(`${number}% [%%%%%%....] 
+  } else if (number == 60) {
+    console.log(`${number}% [%%%%%%....] 
   Still loading...`);
-    } else if (number == 70) {
-      console.log(`${number}% [%%%%%%%...] 
+  } else if (number == 70) {
+    console.log(`${number}% [%%%%%%%...] 
   Still loading...`);
-    } else if (number == 80) {
-      console.log(`${number}% [%%%%%%%%..] 
+  } else if (number == 80) {
+    console.log(`${number}% [%%%%%%%%..] 
   Still loading...`);
-    } else if (number == 90) {
-      console.log(`${number}% [%%%%%%%%%.]
+  } else if (number == 90) {
+    console.log(`${number}% [%%%%%%%%%.]
   Still loading...`);
-    } else if (number == 100) {
-      console.log(`${number}% Complete!
+  } else if (number == 100) {
+    console.log(`${number}% Complete!
   [%%%%%%%%%%]`);
-    }
   }
+}
 
-  loadingBar(50)
+loadingBar(50);
