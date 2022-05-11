af533c4a012164670f0c1d5e3220ca3b70dfe19b
tenkisha <tenkisha@gmail.com>
2022-05-11 19:04:58 +0300

    exercise

--- Fundamentals/Arrays Advanced - Lab/03. First and Last K Numbers.js
+++ Fundamentals/Arrays Advanced - Lab/03. First and Last K Numbers.js
@@ -1,15 +1,11 @@
 function firstLastK(arr) {
-    
-    let k = arr.shift()
- 
-     let firstK = arr.slice(0,k)
-     let lastK = arr.slice(-k)
- 
-     console.log(firstK.join(' '))
-     console.log(lastK.join(' '))
-    
- }
+  let k = arr.shift();
 
- firstLastK([2, 
-    7, 8, 9]
-    )
+  let firstK = arr.slice(0, k);
+  let lastK = arr.slice(-k);
+
+  console.log(firstK.join(" "));
+  console.log(lastK.join(" "));
+}
+
+firstLastK([2, 7, 8, 9]);
---
+++ Fundamentals/Arrays Advanced - Lab/04. Last K Numbers Sequance.js
@@ -0,0 +1,20 @@
+function lastKnumber(n,k){
+
+    let result = [1];
+
+    for(let i = 0;i < n - 1;i++){
+
+        let lastK = result.slice(-k);
+        let sum = 0;
+
+        for(let element of lastK){
+            sum += element;
+        }
+
+        result.push(sum)
+    }
+
+    console.log(result.join(' '))
+}
+
+lastKnumber(6, 3)
