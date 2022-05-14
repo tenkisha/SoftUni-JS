ad0a47aabae40edfa6fbd62d2ffef51f9ac4b77b
tenkisha <tenkisha@gmail.com>
2022-05-14 22:07:15 +0300

    exercise

---
+++ Fundamentals/Objects and Classes - Lab/02. City.js
@@ -0,0 +1,15 @@
+function city(input) {
+  let props = Object.keys(input);
+
+  for (let prop of props) {
+    console.log(`${prop} -> ${input[prop]}`);
+  }
+}
+
+city({
+  name: "Sofia",
+  area: 492,
+  population: 1238438,
+  country: "Bulgaria",
+  postCode: "1000",
+});
