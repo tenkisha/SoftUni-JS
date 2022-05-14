cd3df694172ce5125b40f60d7a41af30c6d7126d
tenkisha <tenkisha@gmail.com>
2022-05-14 22:09:12 +0300

    exercise

---
+++ Fundamentals/Objects and Classes - Lab/04. Convert to JSON.js
@@ -0,0 +1,15 @@
+function convertToJson(name,lastName , hairColor){
+
+    let obj = {
+        name : name,
+        lastName : lastName,
+        hairColor : hairColor
+    };
+
+    let result = JSON.stringify(obj);
+
+    console.log(result)
+
+}
+
+convertToJson('George', 'Jones', 'Brown')
