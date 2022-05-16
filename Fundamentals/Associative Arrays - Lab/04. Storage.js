3919f38f498e692fc93959461f79c2db0a5ff8a9
tenkisha <tenkisha@gmail.com>
2022-05-16 21:06:12 +0300

    exercise

---
+++ Fundamentals/Associative Arrays - Lab/04. Storage.js
@@ -0,0 +1,22 @@
+function solve(input) {
+  let map = new Map();
+  for (let string of input) {
+    let tokens = string.split(" ");
+    let product = tokens[0];
+    let quantity = Number(tokens[1]);
+    if (!map.has(product)) {
+      map.set(product, quantity);
+    } else {
+      let currQuantity = map.get(product);
+      let newQuantity = (currQuantity += quantity);
+      map.set(product, newQuantity);
+    }
+  }
+  for (let [key, quantity] of [...map.entries()]) {
+    console.log(`${key} -> ${quantity}`);
+  }
+}
+solve(['tomatoes 10',
+'coffee 5',
+'olives 100',
+'coffee 40'])
