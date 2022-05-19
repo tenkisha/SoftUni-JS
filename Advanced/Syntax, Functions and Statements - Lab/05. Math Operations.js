4493fa7e384546c334473a3fec20d2cceecc83d4
tenkisha <tenkisha@gmail.com>
2022-05-19 22:17:59 +0300

    exercise

--- Advanced/Syntax, Functions and Statements - Lab/05. Math Operations.js
+++ Advanced/Syntax, Functions and Statements - Lab/05. Math Operations.js
@@ -1,19 +1,19 @@
 function mathOperation(num1, num2, operator) {
-    let result = 0;
-  
-    if (operator == "+") {
-      console.log((result = num1 + num2));
-    } else if (operator == "-") {
-      console.log((result = num1 - num2));
-    } else if (operator == "*") {
-      console.log((result = num1 * num2));
-    } else if (operator == "/") {
-      console.log((result = num1 / num2));
-    }else if(operator == '%'){
-        console.log(result = num1 % num2)
-    }else if(operator == '**'){
-        console.log(result = num1 ** num2)
-    }
-  }
+  let result = 0;
 
-  mathOperation(1,9,'*')
+  if (operator == "+") {
+    console.log((result = num1 + num2));
+  } else if (operator == "-") {
+    console.log((result = num1 - num2));
+  } else if (operator == "*") {
+    console.log((result = num1 * num2));
+  } else if (operator == "/") {
+    console.log((result = num1 / num2));
+  } else if (operator == "%") {
+    console.log((result = num1 % num2));
+  } else if (operator == "**") {
+    console.log((result = num1 ** num2));
+  }
+}
+
+mathOperation(1, 9, "*");
---
+++ Advanced/Syntax, Functions and Statements - Lab/06. Sum of Numbers N…M.js
@@ -0,0 +1,14 @@
+function sum(num1, num2) {
+  let n = Number(num1);
+  let m = Number(num2);
+
+  let sum = 0;
+
+  for (let i = n; i <= m; i++) {
+    sum += i;
+  }
+
+  console.log(sum);
+}
+
+sum(1,5);
