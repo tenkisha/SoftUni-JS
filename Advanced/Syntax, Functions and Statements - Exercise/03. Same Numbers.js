1cd5c8237f123700bcf2b8c6f5457edc5a099915
tenkisha <tenkisha@gmail.com>
2022-05-22 21:53:41 +0300

    exercise

---
+++ Advanced/Syntax, Functions and Statements - Exercise/03. Same Numbers.js
@@ -0,0 +1,22 @@
+function sameNumber(input) {
+  input = String(input);
+  let count = 0;
+  let counter = 0;
+  for (let i = 0; i < input.length; i++) {
+    counter += Number(input[i]);
+    let firstDigit = input[0];
+    if (input[i] == firstDigit) {
+      count++;
+    }
+  }
+
+  if (count == input.length) {
+    console.log("true");
+    console.log(counter);
+  }else{
+      console.log('false')
+      console.log(counter)
+  }
+}
+
+sameNumber(2222222);
