2f99ab24754e6c75e87de4a4c47d9cd6c0124d28
tenkisha <tenkisha@gmail.com>
2022-05-25 23:33:55 +0300

    exercise

---
+++ Advanced/Arrays and Nested Arrays - Lab/01. Even Position Elements.js
@@ -0,0 +1,13 @@
+function evenPosition(input) {
+
+    let arr = []
+  for (let i = 0; i < input.length; i++) {
+    if (i % 2 == 0) {
+      arr.push(input[i])
+    }
+  }
+
+
+  console.log(arr.join(' '))
+}
+evenPosition(["20", "30", "40", "50", "60"]);
