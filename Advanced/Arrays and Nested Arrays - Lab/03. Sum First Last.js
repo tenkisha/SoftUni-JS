a6e128cf28b94c023bd46f4bccb9467775186e34
tenkisha <tenkisha@gmail.com>
2022-05-26 20:12:20 +0300

    exercise

---
+++ Advanced/Arrays and Nested Arrays - Lab/03. Sum First Last.js
@@ -0,0 +1,12 @@
+function sumFirstLast(input){
+
+    let first = Number(input.shift())
+    let last = Number(input.pop())
+
+    let sum = first + last
+
+    console.log(sum)
+
+}
+
+sumFirstLast(['20', '30', '40'])
