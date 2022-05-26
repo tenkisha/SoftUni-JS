6e701d6d69b6f5d0d1e6bb1d33932a8568e8aea3
tenkisha <tenkisha@gmail.com>
2022-05-27 00:02:30 +0300

    exercise

---
+++ Advanced/Arrays and Nested Arrays - Lab/07. Piece of Pie.js
@@ -0,0 +1,15 @@
+function pie(arr, start, finish) {
+  return arr.slice(arr.indexOf(start), arr.indexOf(finish) + 1);
+}
+
+console.log(pie(
+  [
+    "Pumpkin Pie",
+    "Key Lime Pie",
+    "Cherry Pie",
+    "Lemon Meringue Pie",
+    "Sugar Cream Pie",
+  ],
+  "Key Lime Pie",
+  "Lemon Meringue Pie"
+));
