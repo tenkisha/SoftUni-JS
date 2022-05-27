eef8afded338366077c55cfc583db7af961ad152
tenkisha <tenkisha@gmail.com>
2022-05-27 22:09:56 +0300

    exercise

--- Advanced/Arrays and Nested Arrays - Lab/10. Diagonal Sums.js
+++ Advanced/Arrays and Nested Arrays - Lab/10. Diagonal Sums.js
@@ -1,12 +1,12 @@
 function diagonalSum(input) {
-    let mainDiagonal = 0;
-    let secondaryDiagonal = 0;
- 
-    for (let i = 0; i < input.length; i++) {
-        mainDiagonal += input[i][i];
-        secondaryDiagonal += input[i][input.length - 1 - i];
-    }
-    console.log(mainDiagonal, secondaryDiagonal);
+  let mainDiagonal = 0;
+  let secondaryDiagonal = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    mainDiagonal += input[i][i];
+    secondaryDiagonal += input[i][input.length - 1 - i];
+  }
+  console.log(mainDiagonal, secondaryDiagonal);
 }
 
 diagonalSum([
