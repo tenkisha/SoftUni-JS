08e65f88ac84bc57eb650ba870b8075f58e95b05
tenkisha <tenkisha@gmail.com>
2022-05-29 20:12:29 +0300

    exercise

--- Advanced/Arrays and Nested Arrays - Exercise/08. Sort an Array by 2 Criteria.js
+++ Advanced/Arrays and Nested Arrays - Exercise/08. Sort an Array by 2 Criteria.js
@@ -1,23 +1,15 @@
 function sortArray(input) {
-    input.sort(twoCriteriaSort);
-    input.forEach(el => console.log(el));
-  
-    function twoCriteriaSort(cur, next) {
-      if (cur.length === next.length) {
-        return cur.localeCompare(next);
-      }
-      return cur.length - next.length;
+  input.sort(twoCriteriaSort);
+  input.forEach((el) => console.log(el));
+
+  function twoCriteriaSort(cur, next) {
+    if (cur.length === next.length) {
+      return cur.localeCompare(next);
     }
+    return cur.length - next.length;
   }
+}
 
-sortArray(["alpha", "beta", "gamma"]);
+console.log("-----");
 
-console.log('-----')
-
-sortArray(['Isacc', 
-'Theodor', 
-'Jack', 
-'Harrison', 
-'George']
-
-)
+sortArray(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
