5c4338cd31c3adf54c038400ee643e1f0bfaee24
tenkisha <tenkisha@gmail.com>
2022-05-29 20:04:47 +0300

    exercise

--- Advanced/Arrays and Nested Arrays - Exercise/07. Sorting Numbers.js
+++ Advanced/Arrays and Nested Arrays - Exercise/07. Sorting Numbers.js
@@ -1,12 +1,11 @@
-function sortNumbers(input){
-    let result = [];
-    array = input.sort((a, b) => a - b);
-    while(array.length !== 0){
-      result.push(array[0]) && array.shift();
-      result.push(array[array.length-1]) && array.pop
-      ();
-    }
- console.log(result.join('\n'));
- }
+function sortNumbers(input) {
+  let result = [];
+  array = input.sort((a, b) => a - b);
+  while (array.length !== 0) {
+    result.push(array[0]) && array.shift();
+    result.push(array[array.length - 1]) && array.pop();
+  }
+  return result;
+}
 
-sortNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56])
+sortNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]);
---
+++ Advanced/Arrays and Nested Arrays - Exercise/08. Sort an Array by 2 Criteria.js
@@ -0,0 +1,23 @@
+function sortArray(input) {
+    input.sort(twoCriteriaSort);
+    input.forEach(el => console.log(el));
+  
+    function twoCriteriaSort(cur, next) {
+      if (cur.length === next.length) {
+        return cur.localeCompare(next);
+      }
+      return cur.length - next.length;
+    }
+  }
+
+sortArray(["alpha", "beta", "gamma"]);
+
+console.log('-----')
+
+sortArray(['Isacc', 
+'Theodor', 
+'Jack', 
+'Harrison', 
+'George']
+
+)
