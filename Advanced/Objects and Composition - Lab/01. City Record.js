2dbe88a8ab05577ac71035c25a63391b3875bc47
tenkisha <tenkisha@gmail.com>
2022-05-30 15:36:29 +0300

    exercise

---
+++ Advanced/Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,12 @@
+function cityRecords(name,population,treasury) {
+
+    let person = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+
+    console.log(person);
+}
+
+cityRecords("Tortuga", 7000, 15000);
