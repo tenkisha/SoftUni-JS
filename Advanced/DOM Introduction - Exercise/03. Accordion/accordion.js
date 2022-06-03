0459ac63d3ab967befeb5aae36a6df2f54d065c6
tenkisha <tenkisha@gmail.com>
2022-06-03 23:35:43 +0300

    exercise

--- Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ Advanced/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,17 +1,12 @@
 function toggle() {
-    
-  const button = document.querySelector('.button')
-  const text = document.querySelector('#extra')
+  const button = document.querySelector(".button");
+  const text = document.querySelector("#extra");
 
-  if(button.textContent === 'More'){
-      button.textContent = 'Less'
-      text.style.display = 'block'
-  }else{
-      button.textContent = 'More'
-      text.style.display = 'none'
+  if (button.textContent === "More") {
+    button.textContent = "Less";
+    text.style.display = "block";
+  } else {
+    button.textContent = "More";
+    text.style.display = "none";
   }
-
-
-  console.log(text)
-
 }
