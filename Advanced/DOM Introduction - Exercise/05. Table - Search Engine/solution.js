82af95fa5a55857b0293006a966822db899fb710
tenkisha <tenkisha@gmail.com>
2022-06-03 23:58:16 +0300

    exercise

--- Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ Advanced/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let input = document.getElementById('searchField');
+   let rows = document.querySelectorAll('tbody tr');
 
    function onClick() {
-      //   TODO:
+
+      for(let row of rows){
+         row.classList.remove('select')
+      }
+
+      for(let row of rows){
+         if(row.value !== '' && row.innerHTML.includes(input.value)){
+            row.className = 'select'
+         }
+      }
 
    }
 }
