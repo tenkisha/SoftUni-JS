7c5d66afb75112cd946b108b0cfe9a6adaaceae0
tenkisha <tenkisha@gmail.com>
2022-06-05 17:03:55 +0300

    exercise

--- Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ Advanced/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  let textInput = document.getElementById("input");
+  let textArr = textInput.value.split(".").filter((s) => s !== "");
+  let resultDiv = document.getElementById("output");
+
+  while (textArr.length > 0) {
+    let text = textArr.splice(0, 3) + '.'
+    let p = document.createElement("p");
+    p.textContent = text;
+    resultDiv.appendChild(p);
+  }
 }
