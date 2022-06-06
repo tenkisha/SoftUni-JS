fca45f2815b460a75aadd98753effea56038423c
tenkisha <tenkisha@gmail.com>
2022-06-06 23:06:22 +0300

    exercise

--- Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ Advanced/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+  const input = document.getElementById("newItemText");
+
+  const liElement = document.createElement("li");
+  liElement.textContent = input.value;
+
+  const deleteBtn = document.createElement("a");
+  deleteBtn.textContent = "[Delete]";
+  deleteBtn.href = "#";
+  liElement.appendChild(deleteBtn);
+
+  deleteBtn.addEventListener("click", onDelete);
+
+  document.getElementById("items").appendChild(liElement);
+
+  input.value = "";
+}
+
+function onDelete(event) {
+  event.target.parentElement.remove();
 }
