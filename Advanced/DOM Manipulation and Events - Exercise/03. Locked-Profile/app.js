8742b704acbfd68e7ae85c99ee676109326d83d5
tenkisha <tenkisha@gmail.com>
2022-06-29 12:11:08 +0300

    exercise

--- Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ Advanced/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,18 +1,18 @@
 function lockedProfile() {
- 
-    Array.from(document.querySelectorAll('.profile button'))
-    .forEach(button => button.addEventListener('click',onClick));
 
-    function onClick(ev){
+    Array.from(document.querySelectorAll('.profile button'))
+        .forEach(button => button.addEventListener('click', onClick));
+
+    function onClick(ev) {
         let profile = ev.target.parentElement;
         let isActive = profile.querySelector('input[value="unlock"]').checked;
-        if(isActive){
+        if (isActive) {
             let info = Array.from(profile.querySelectorAll('div'))
-            .find(p => p.id.includes('HiddenFields'));
+                .find(p => p.id.includes('HiddenFields'));
 
-            if(ev.target.textContent === 'Show more') {
-                ev.target.textContent = 'Hide it' ;
-                info.style.display = 'block' ;
+            if (ev.target.textContent === 'Show more') {
+                ev.target.textContent = 'Hide it';
+                info.style.display = 'block';
 
             } else {
 
--- Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ Advanced/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+   let newText = document.getElementById('newItemText').value;
+   let newValue = document.getElementById('newItemValue').value;
+
+   let option = document.createElement('option');
+   option.text = newText;
+   option.value = newValue;
+
+   let menu = document.getElementById('menu');
+
+   if(newText !== '' && newValue !== ''){
+
+       menu.appendChild(option);
+   } 
+
+   document.getElementById('newItemText').value = '';
+   document.getElementById('newItemValue').value = '';
+
 }
