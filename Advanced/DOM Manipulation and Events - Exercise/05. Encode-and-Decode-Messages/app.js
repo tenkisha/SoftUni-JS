f732bbd473b248c6a8504dcb321dd55f042fca73
tenkisha <tenkisha@gmail.com>
2022-07-01 18:10:27 +0300

    exercise

--- Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let encodeTextArea = document.querySelector('#main div textarea');
+    let decodeTextArea = document.querySelector('#main div:nth-child(2) textarea');
+
+
+
+    document.querySelector('#main div button').addEventListener('click', onEncode);
+
+    document.querySelector('#main div:nth-child(2) button').addEventListener('click', onDecode);
+
+    function onEncode() {
+        let msgToEncode = encodeTextArea.value.split('');
+        let encodedMsg = msgToEncode.map(e => String.fromCharCode(e.charCodeAt() + 1)).join('');
+        encodeTextArea.value = '';
+
+        decodeTextArea.value = encodedMsg;
+
+    }
+
+    function onDecode() {
+        let msgToDecode = decodeTextArea.value.split('');
+        let decodedMsg = msgToDecode.map(e => String.fromCharCode(e.charCodeAt() - 1)).join('');
+
+        decodeTextArea.value = decodedMsg;
+    }
 }
